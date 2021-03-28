7c1d30a7d7406ab5fde5c31fb5829c11a2167d39
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-03-28 11:27:49 -0300

    Creating method to manipulate Livros and Estoque

--- livro.rb
+++ livro.rb
@@ -1,16 +1,35 @@
 class Livro
   attr_reader :titulo, :ano_lancamento, :preco
 
-  def initialize(titulo, preco, ano_lancamento)
+  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
     @titulo = titulo
     @ano_lancamento = ano_lancamento
+    @possui_reimpressao = possui_reimpressao
     @preco = calcula_preco(preco)
+  end
+
+  def possui_reimpressao?
+    @possui_reimpressao
+  end
+
+  def to_csv
+    "#{@titulo},#{@ano_lancamento},#{@preco}"
   end
 
   private
 
   def calcula_preco(base)
-    @ano_lancamento < 2000 ? base *= 0.7 : base
+    if @ano_lancamento < 2006
+      if @possui_reimpressao
+        base * 0.9
+      else
+        base * 0.95
+      end
+    elsif @ano_lancamento <= 2010
+      base * 0.96
+    else
+      base
+    end
   end
 end
 
@@ -19,9 +38,49 @@ def livro_para_newsletter(livro)
     puts 'Newsletter/Liquidação'
     puts livro.titulo
     puts livro.preco
+    puts livro.possui_reimpressao?
   end
 end
 
-algoritmos = Livro.new('Algoritmos', 100, 1998)
+class Estoque
+  def initialize
+    @livros = []
+  end
 
-livro_para_newsletter(algoritmos)
+  def exporta_csv
+    @livros.each do |livro|
+      puts livro.to_csv
+    end
+  end
+
+  def mais_baratos_que(valor)
+    @livros.select do |livro|
+      livro.preco <= valor
+    end
+  end
+
+  def total
+    @livros.size
+  end
+
+  def adiciona(livro)
+    @livros << livro if livro
+  end
+end
+
+algoritmos = Livro.new('Algoritmos', 100, 1998, true)
+arquitetura = Livro.new('Introdução a Arquitetura e Design de Software', 70, 2011, true)
+
+estoque = Estoque.new
+estoque.adiciona algoritmos
+estoque.adiciona arquitetura
+estoque.adiciona Livro.new('The Pragmatic Programmer', 100, 1999, true)
+estoque.adiciona Livro.new('Programming Ruby', 100, 2004, true)
+estoque.adiciona nil
+
+estoque.exporta_csv
+
+baratos = estoque.mais_baratos_que 80
+baratos.each do |livro|
+  puts livro.titulo
+end
