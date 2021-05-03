36fb1380d04e22d0e850bb8d5651591cac612f25
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-05-03 18:59:02 -0300

    Refactoring methods reusing them

--- sistema.rb
+++ sistema.rb
@@ -16,15 +16,18 @@ class Conversor
   end
 end
 
-algoritmos = Livro.new('Algoritmos', 100, 1998, true, '')
-arquitetura = Livro.new('Introdução a Arquitetura e Design de Software', 70, 2011, true, '')
-programmer = Livro.new('The Pragmatic Programmer', 100, 1999, true, '')
-ruby = Livro.new('Programming Ruby', 100, 2004, true, '')
+algoritmos = Livro.new('Algoritmos', 100, 1998, true, '', 'livro')
+arquitetura = Livro.new('Introdução a Arquitetura e Design de Software', 70, 2011, true, '', 'livro')
+programmer = Livro.new('The Pragmatic Programmer', 100, 1999, true, '', 'livro')
+ruby = Livro.new('Programming Ruby', 100, 2004, true, '', 'livro')
+revistona = Livro.new('Revista de Ruby', 10, 2012, true, 'Revistas', 'revista')
 
 estoque = Estoque.new
-estoque << algoritmos << algoritmos << ruby << programmer << arquitetura << ruby << ruby
+estoque << algoritmos << algoritmos << ruby << programmer << arquitetura << ruby << ruby << revistona << revistona
 estoque.vende ruby
 estoque.vende algoritmos
 estoque.vende algoritmos
+estoque.vende revistona
 
 puts estoque.livro_que_mais_vendeu_por_titulo.titulo
+puts estoque.revista_que_mais_vendeu_por_titulo.titulo
